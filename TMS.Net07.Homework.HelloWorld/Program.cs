619042d047ea6e659101a895cd528d7506889100
unknown <malenkayakoshechka17@gmail.com>
2021-01-28 13:12:23 +0300

    easy task

---
+++ TMS.Net07.Homework.HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numer: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
